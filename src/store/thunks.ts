9f2d240df27ba816142f1184e9683bec19263c00
Vladislav Taranov <vladislav.taranov@live.com>
2020-10-08 11:49:02 +0300

    Changed thunks signature

--- src/store/thunks.ts
+++ src/store/thunks.ts
@@ -1,4 +1,6 @@
-import { Dispatch } from 'redux';
+import { Action, Dispatch } from 'redux';
+import { ThunkAction } from 'redux-thunk';
+import { State } from './storeTypes';
 import actions from './actions';
 import Server from '../services/server';
 
@@ -8,28 +10,29 @@ const {
   fetchTicketsFailure
 } = actions;
 
-const fetchTickets = () => async (dispatch: Dispatch) => {
-  dispatch(fetchTicketsRequest());
-  const server: Server = new Server();
-  let searchId: string;
+const fetchTickets =
+  (): ThunkAction<void, State, unknown, Action> => async (dispatch: Dispatch) => {
+    dispatch(fetchTicketsRequest());
+    const server: Server = new Server();
+    let searchId: string;
 
-  const sendRequest = async () => {
+    const sendRequest = async () => {
+      try {
+        const { tickets, isStop } = await server.getTickets(searchId);
+        dispatch(fetchTicketsSuccess(tickets));
+        if (!isStop) await sendRequest();
+      } catch (error) {
+        await sendRequest();
+      }
+    };
+
     try {
-      const { tickets, isStop } = await server.getTickets(searchId);
-      dispatch(fetchTicketsSuccess(tickets));
-      if (!isStop) await sendRequest();
-    } catch (error) {
+      searchId = await server.getSearchId();
       await sendRequest();
+    } catch (error) {
+      dispatch(fetchTicketsFailure());
     }
   };
-
-  try {
-    searchId = await server.getSearchId();
-    await sendRequest();
-  } catch (error) {
-    dispatch(fetchTicketsFailure());
-  }
-};
 
 export default {
   fetchTickets
